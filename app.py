1a9e56480b9829e102804fe0f29dce622a12bb97
Mohamed Ashmawy <ashma010@umn.edu>
2018-04-16 00:10:05 -0400

    Arabic encoding

--- app.py
+++ app.py
@@ -30,7 +30,9 @@ class all_deroos(Resource):
 		result = deroos_schema.dump(deroos)
 		for x in result.data:
 			for y in x:
+				print (y)
 				y.encode('utf-8')
+				print (y)
 		return jsonify({'deroos': result.data})
 
 class active_deroos(Resource):
